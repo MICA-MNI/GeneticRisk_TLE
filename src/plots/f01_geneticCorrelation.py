218f90aa51df986f529ca2dbec320caac64338ab
alexander-ngo <alexander.ngo@mail.mcgill.ca>
2024-09-06 11:13:35 -0400

    Add plotting scripts

---
+++ src/plots/f01_geneticCorrelation.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import pickle
+
+# Main analysis
+def main():
+
+    print('-----------------------------')
+    print('Brain-wide effects of PRS-TLE')
+    print('-----------------------------')
+
+    # Load results
+
+    # Plot results
+
+
+    print()
+    print('-------------------------')
+    print('Local effects of PRS-TLE')
+    print('------------------------')
+
+    # Load results
+
+    # Plot results
+
+
+if __name__ == "__main__":
+    main()
