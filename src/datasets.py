b85dedb37f6413f3a09023128dba418da30cdf62
Alex Ngo <alexngo@macbook.local>
2024-09-12 15:54:58 -0400

    Update script

--- src/datasets.py
+++ src/datasets.py
@@ -1,3 +1,4 @@
+import glob
 import pandas as pd
 import numpy as np
 from neuroCombat import neuroCombat
@@ -129,9 +130,14 @@ def main():
     )
     categorical_cols = ["sex", "group"]
     batch_col = "dataset"
-    ct = neuroCombat(
-        dat=ct, covars=covars, batch_col=batch_col, categorical_cols=categorical_cols
-    )["data"]
+    ct = np.transpose(
+        neuroCombat(
+            dat=ct,
+            covars=covars,
+            batch_col=batch_col,
+            categorical_cols=categorical_cols,
+        )["data"]
+    )
 
     print()
     print("Save data")
@@ -182,7 +188,7 @@ def main():
     print("Thickness data")
     print("-------------------------")
     multi_tle_ct = pd.read_csv("../data/raw/multi_tle_ct.csv", index_col="participant")
-    ct = np.transpose(multi_tle_ct.to_numpy())
+    ct = multi_tle_ct.to_numpy()
 
     print()
     print("Save data")
@@ -191,10 +197,124 @@ def main():
         "../data/processed/multi_tle_data",
         age=age,
         sex=sex,
-        dataset=dataset,
+        site=site,
         group=group,
         focus=focus,
         ct=ct,
+    )
+
+    print()
+    print("-------------------------")
+    print("Loading multi IGE dataset")
+    print("-------------------------")
+
+    print()
+    print("Demographic data")
+    print("-------------------------")
+    multi_ige_demographics = pd.read_csv(
+        "../data/raw/multi_ige_demographics.csv", index_col="participant"
+    )
+    age = multi_ige_demographics["age"]
+    sex = multi_ige_demographics["sex"]
+    group = multi_ige_demographics["group"]
+    site = multi_ige_demographics["site"]
+    print(f"# of HC: {np.sum(group=='HC')}; # IGE: {np.sum(group=='IGE')}")
+    print(
+        f"mean age HC: {np.nanmean(age[group=='HC'])}; std age HC: {np.nanstd(age[group=='HC'])}; range age HC: {np.nanmin(age[group=='HC'])} - {np.nanmax(age[group=='HC'])}"
+    )
+    print(
+        f"mean age IGE: {np.nanmean(age[group=='IGE'])}; std age IGE: {np.nanstd(age[group=='IGE'])}; range age IGE: {np.nanmin(age[group=='IGE'])} - {np.nanmax(age[group=='IGE'])}"
+    )
+    print(
+        f"# of M HC: {np.sum((sex=='M') & (group=='HC'))}; # of F HC: {np.sum((sex=='F') & (group=='HC'))}"
+    )
+    print(
+        f"# of M IGE: {np.sum((sex=='M') & (group=='IGE'))}; # of F IGE: {np.sum((sex=='F') & (group=='IGE'))}"
+    )
+
+    print()
+    print("Thickness data")
+    print("-------------------------")
+    multi_ige_ct = pd.read_csv("../data/raw/multi_ige_ct.csv", index_col="participant")
+    ct = np.transpose(multi_ige_ct.to_numpy())
+
+    print()
+    print("Save data")
+    print("-------------------------")
+    np.savez(
+        "../data/processed/multi_ige_data",
+        age=age,
+        sex=sex,
+        site=site,
+        group=group,
+        ct=ct,
+    )
+
+    print()
+    print("-----------------------")
+    print("Loading HCP connectomes")
+    print("-----------------------")
+
+    print()
+    print("Functional connectomes")
+    print("-----------------------")
+    # Load files
+    files = glob.glob("../data/raw/connectomes/*FC*")
+
+    # Compute average connectome
+    for i, f in enumerate(files):
+        mtx = np.loadtxt(f, delimiter=",")
+        mtx_z = np.arctanh(mtx)
+        mtx_z[(mtx_z < 0) | np.isinf(mtx_z)] = 0
+        mtx_z = np.triu(mtx_z, 1) + mtx_z.T
+
+        if i == 0:
+            fc = np.empty((mtx_z.shape[0], mtx_z.shape[1], len(files)))
+
+        fc[:, :, i] = mtx_z
+
+    mean_fc = np.mean(fc, axis=2)
+
+    # Isolate cortico-cortical and cortico-subcortical functional connectivity
+    ctx_idx = np.concatenate(
+        [np.arange(49, 52), np.arange(53, 84), np.arange(84, 87), np.arange(88, 119)]
+    )
+    sctx_idx = np.array([6, 5, 1, 4, 3, 2, 0, 13, 12, 8, 11, 10, 9, 7])
+    fc_ctx = mean_fc[np.ix_(ctx_idx, ctx_idx)]
+    fc_sctx = mean_fc[np.ix_(sctx_idx, ctx_idx)]
+
+    print()
+    print("Structural connectomes")
+    print("-----------------------")
+    # Load files
+    files = glob.glob("../data/raw/connectomes/*SC*")
+
+    # Compute average connectome
+    for i, f in enumerate(files):
+        mtx = np.loadtxt(f, delimiter=",")
+        mtx_log = np.log(np.triu(mtx, 1) + mtx.T)
+        mtx_log[np.isinf(mtx_log)] = 0
+
+        if i == 0:
+            sc = np.empty((mtx_log.shape[0], mtx_log.shape[1], len(files)))
+
+        sc[:, :, i] = mtx_z
+
+    mean_sc = np.mean(sc, axis=2)
+
+    # Isolate cortico-cortical and cortico-subcortical structural connectivity
+    sc_ctx = mean_sc[np.ix_(ctx_idx, ctx_idx)]
+    sc_sctx = mean_sc[np.ix_(sctx_idx, ctx_idx)]
+
+    print()
+    print("Save data")
+    print("-----------------------")
+    np.savez(
+        "../data/processed/hcp_data",
+        fc_ctx=fc_ctx,
+        fc_sctx=fc_sctx,
+        sc_ctx=sc_ctx,
+        sc_sctx=sc_sctx,
     )
 
 
