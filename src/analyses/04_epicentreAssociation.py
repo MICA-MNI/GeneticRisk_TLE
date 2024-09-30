1be8c325502fdc99f622b3cdd2c252b3b96f5e0e
Alex Ngo <alexngo@macbook.local>
2024-09-30 12:49:18 -0400

    Update script

--- src/analyses/04_epicentreAssociation.py
+++ src/analyses/04_epicentreAssociation.py
@@ -26,14 +26,14 @@ def main():
         atrophy_map = atrophy[f"{hemi.lower()}tle"].t
         fc_ctx_r, fc_ctx_p = util.epicenter_mapping(atrophy_map, fc_ctx)
         fc_sctx_r, fc_sctx_p = util.epicenter_mapping(atrophy_map, fc_sctx)
-        sc_ctx_r, sc_ctx_p = util.epicenter_mapping(atrophy_map, sc_ctx)
-        sc_sctx_r, sc_sctx_p = util.epicenter_mapping(atrophy_map, sc_sctx)
-        epicentre[f"{hemi.lower()}tle"] = {
-            "fc_ctx": {"r": fc_ctx_r, "p": fc_ctx_p},
-            "fc_sctx": {"r": fc_ctx_r, "p": fc_sctx_p},
-            "sc_ctx": {"r": sc_ctx_r, "p": sc_ctx_p},
-            "sc_sctx": {"r": sc_sctx_r, "p": sc_sctx_p},
-        }
+        # sc_ctx_r, sc_ctx_p = util.epicenter_mapping(atrophy_map, sc_ctx)
+        # sc_sctx_r, sc_sctx_p = util.epicenter_mapping(atrophy_map, sc_sctx)
+        # epicentre[f"{hemi.lower()}tle"] = {
+        #    "fc_ctx": {"r": fc_ctx_r, "p": fc_ctx_p},
+        #    "fc_sctx": {"r": fc_ctx_r, "p": fc_sctx_p},
+        #    "sc_ctx": {"r": sc_ctx_r, "p": sc_ctx_p},
+        #    "sc_sctx": {"r": sc_sctx_r, "p": sc_sctx_p},
+        # }
 
         print()
         print("Correlate with PRS epicentre map")
@@ -41,11 +41,13 @@ def main():
         r_fc_epi, p_fc_epi, null_fc_epi = util.spatial_correlation(
             np.concatenate((fc_ctx_r, fc_sctx_r)),
             np.concatenate((prs_fc_ctx, prs_fc_sctx)),
+            surface_name="fsa5_with_sctx",
             parcellation_name="aparc_aseg",
         )
         r_sc_epi, p_sc_epi, null_sc_epi = util.spatial_correlation(
             np.concatenate((sc_ctx_r, sc_sctx_r)),
             np.concatenate((prs_sc_ctx, prs_sc_sctx)),
+            surface_name="fsa5_with_sctx",
             parcellation_name="aparc_aseg",
         )
 
