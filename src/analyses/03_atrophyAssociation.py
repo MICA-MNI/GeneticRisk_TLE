62332c6e0524d5b1b10c1939b1fda812378852fd
Alex Ngo <alexngo@macbook.local>
2024-12-12 13:35:27 -0500

    Updated analyses

--- src/analyses/03_atrophyAssociation.py
+++ src/analyses/03_atrophyAssociation.py
@@ -8,7 +8,7 @@ import utilities as util
 # Helper functions
 def load_local_tle():
     """
-    Load local TLE (EpiC x MICs x NKG) data
+    Load local TLE (MICs x NKG) data
 
     Returns:
     numpy.ndarray: The loaded data containing age, sex, dataset, focus, and ct information.
@@ -19,51 +19,66 @@ def load_local_tle():
     )
 
 
+def load_multi_tle():
+    """
+    Load multisite TLE (ENIGMA) data
+
+    Returns:
+    numpy.ndarray: The loaded data containing age, sex, dataset, group, focus, and ct information.
+    """
+    return util.load_data(
+        "../../data/processed/multi_tle_data.npz",
+        ["age", "sex", "site", "group", "focus", "ct"],
+    )
+
+
 # Main analysis
 def main():
-    # Load LTE data
-    age, sex, focus, ct = load_local_tle()
+    load_dataset = {"local": load_local_tle, "multi": load_multi_tle}
+    for key in load_dataset:
+        # Load LTE data
+        age, sex, focus, ct = load_dataset[key]()
 
-    # Load imaging genetic result
-    imaging_genetics = util.load_imaging_genetic()
+        # Load imaging genetic result
+        imaging_genetics = util.load_imaging_genetic()
 
-    x = util.zscore_flip(ct, focus, "C", "_")
-    covar = pd.DataFrame({"age": age, "sex": sex})
-    atrophy, association = {}, {}
-    for hemi in ["L", "R"]:
+        x = util.zscore_flip(ct, focus, "C", "_")
+        covar = pd.DataFrame({"age": age, "sex": sex})
+        atrophy, association = {}, {}
+        for hemi in ["L", "R"]:
+            print()
+            print("------------------------")
+            print(f"{hemi} temporal lobe epilepsy")
+            print("------------------------")
+
+            print()
+            print("Derive atrophy map")
+            print("------------------------")
+            slm = util.casecontrol_difference(x, covar, focus, "C", hemi)
+            atrophy[f"{hemi.lower()}tle"] = slm
+
+            print()
+            print("Correlate with PRS map")
+            print("------------------------")
+            r, p, null = util.spatial_correlation(slm.t, imaging_genetics.t)
+
+            association[f"{hemi.lower()}tle"] = {"r": r, "p": p, "null": null}
+
+            print(f"Correlation: r = {r}, p = {p}")
+
         print()
-        print("------------------------")
-        print(f"{hemi} temporal lobe epilepsy")
-        print("------------------------")
+        print("------------")
+        print("Save results")
+        print("------------")
+        util.save_to_pickle(
+            f"../../data/results/03_atrophyAssociation/{key}_atrophy.pkl",
+            {"atrophy": atrophy},
+        )
 
-        print()
-        print("Derive atrophy map")
-        print("------------------------")
-        slm = util.casecontrol_difference(x, covar, focus, "C", hemi)
-        atrophy[f"{hemi.lower()}tle"] = slm
-
-        print()
-        print("Correlate with PRS map")
-        print("------------------------")
-        r, p, null = util.spatial_correlation(slm.t, imaging_genetics.t)
-
-        association[f"{hemi.lower()}tle"] = {"r": r, "p": p, "null": null}
-
-        print(f"Correlation: r = {r}, p = {p}")
-
-    print()
-    print("------------")
-    print("Save results")
-    print("------------")
-    util.save_to_pickle(
-        "../../data/results/03_atrophyAssociation/local_atrophy.pkl",
-        {"atrophy": atrophy},
-    )
-
-    util.save_to_pickle(
-        "/../../data/results/03_atrophyAssociation/local_association.pkl",
-        {"association": association},
-    )
+        util.save_to_pickle(
+            f"/../../data/results/03_atrophyAssociation/{key}_association.pkl",
+            {"association": association},
+        )
 
 
 if __name__ == "__main__":
--- src/analyses/04_epicentreAssociation.py
+++ src/analyses/04_epicentreAssociation.py
@@ -4,82 +4,89 @@ import utilities as util
 
 # Main analysis
 def main():
-    atrophy = util.load_result(
-        "../../data/results/03_atrophyAssociation/local_atrophy.pkl", ["atrophy"]
-    )[0]
-
     fc_ctx, fc_sctx, sc_ctx, sc_sctx = util.load_connectomes()
     prs_fc_ctx, prs_fc_sctx, prs_sc_ctx, prs_sc_sctx = util.load_imaging_genetic(
         "network"
     )
 
-    epicentre = {}
-    association = {}
-    for hemi in ["L", "R"]:
+    for site in ["local", "multi"]:
         print("------------------------")
-        print(f"{hemi} temporal lobe epilepsy")
+        print(f"Dataset: {site}")
         print("------------------------")
+
+        # Load atrophy maps
+        atrophy = util.load_result(
+            f"../../data/results/03_atrophyAssociation/{site}_atrophy.pkl", ["atrophy"]
+        )[0]
+
+        epicentre = {}
+        association = {}
+        for hemi in ["L", "R"]:
+            print()
+            print("------------------------")
+            print(f"{hemi} temporal lobe epilepsy")
+            print("------------------------")
+
+            print()
+            print("Derive disease epicentre map")
+            print("------------------------")
+            atrophy_map = atrophy[f"{hemi.lower()}tle"].t
+            fc_ctx_r, fc_ctx_p = util.epicenter_mapping(atrophy_map, fc_ctx)
+            fc_sctx_r, fc_sctx_p = util.epicenter_mapping(atrophy_map, fc_sctx)
+            sc_ctx_r, sc_ctx_p = util.epicenter_mapping(atrophy_map, sc_ctx)
+            sc_sctx_r, sc_sctx_p = util.epicenter_mapping(atrophy_map, sc_sctx)
+            epicentre[f"{hemi.lower()}tle"] = {
+                "fc_ctx": {"r": fc_ctx_r, "p": fc_ctx_p},
+                "fc_sctx": {"r": fc_ctx_r, "p": fc_sctx_p},
+                "sc_ctx": {"r": sc_ctx_r, "p": sc_ctx_p},
+                "sc_sctx": {"r": sc_sctx_r, "p": sc_sctx_p},
+            }
+
+            print()
+            print("Correlate with PRS epicentre map")
+            print("------------------------")
+            r_fc_epi, p_fc_epi, null_fc_epi = util.spatial_correlation(
+                np.concatenate((fc_ctx_r, fc_sctx_r)),
+                np.concatenate((prs_fc_ctx, prs_fc_sctx)),
+                surface_name="fsa5_with_sctx",
+                parcellation_name="aparc_aseg",
+            )
+            r_sc_epi, p_sc_epi, null_sc_epi = util.spatial_correlation(
+                np.concatenate((sc_ctx_r, sc_sctx_r)),
+                np.concatenate((prs_sc_ctx, prs_sc_sctx)),
+                surface_name="fsa5_with_sctx",
+                parcellation_name="aparc_aseg",
+            )
+
+            association[f"{hemi.lower()}tle"] = {
+                "fc_epi": {
+                    "r_fc_epi": r_fc_epi,
+                    "p_fc_epi": p_fc_epi,
+                    "null_fc_epi": null_fc_epi,
+                },
+                "sc_epi": {
+                    "r_sc_epi": r_sc_epi,
+                    "p_sc_epi": p_sc_epi,
+                    "null_sc_epi": null_sc_epi,
+                },
+            }
+
+            print(f"FC epicentre correlation: r = {r_fc_epi}, p = {p_fc_epi}")
+            print(f"SC epicentre correlation: r = {r_sc_epi}, p = {p_sc_epi}")
 
         print()
-        print("Derive disease epicentre map")
-        print("------------------------")
-        atrophy_map = atrophy[f"{hemi.lower()}tle"].t
-        fc_ctx_r, fc_ctx_p = util.epicenter_mapping(atrophy_map, fc_ctx)
-        fc_sctx_r, fc_sctx_p = util.epicenter_mapping(atrophy_map, fc_sctx)
-        sc_ctx_r, sc_ctx_p = util.epicenter_mapping(atrophy_map, sc_ctx)
-        sc_sctx_r, sc_sctx_p = util.epicenter_mapping(atrophy_map, sc_sctx)
-        epicentre[f"{hemi.lower()}tle"] = {
-            "fc_ctx": {"r": fc_ctx_r, "p": fc_ctx_p},
-            "fc_sctx": {"r": fc_ctx_r, "p": fc_sctx_p},
-            "sc_ctx": {"r": sc_ctx_r, "p": sc_ctx_p},
-            "sc_sctx": {"r": sc_sctx_r, "p": sc_sctx_p},
-        }
-
-        print()
-        print("Correlate with PRS epicentre map")
-        print("------------------------")
-        r_fc_epi, p_fc_epi, null_fc_epi = util.spatial_correlation(
-            np.concatenate((fc_ctx_r, fc_sctx_r)),
-            np.concatenate((prs_fc_ctx, prs_fc_sctx)),
-            surface_name="fsa5_with_sctx",
-            parcellation_name="aparc_aseg",
-        )
-        r_sc_epi, p_sc_epi, null_sc_epi = util.spatial_correlation(
-            np.concatenate((sc_ctx_r, sc_sctx_r)),
-            np.concatenate((prs_sc_ctx, prs_sc_sctx)),
-            surface_name="fsa5_with_sctx",
-            parcellation_name="aparc_aseg",
+        print("------------")
+        print("Save results")
+        print("------------")
+        util.save_to_pickle(
+            f"../../data/results/04_epicentreAssociation/{site}_epicentre.pkl",
+            {"epicentre": epicentre},
         )
 
-        association[f"{hemi.lower()}tle"] = {
-            "fc_epi": {
-                "r_fc_epi": r_fc_epi,
-                "p_fc_epi": p_fc_epi,
-                "null_fc_epi": null_fc_epi,
-            },
-            "sc_epi": {
-                "r_sc_epi": r_sc_epi,
-                "p_sc_epi": p_sc_epi,
-                "null_sc_epi": null_sc_epi,
-            },
-        }
-
-        print(f"FC epicentre correlation: r = {r_fc_epi}, p = {p_fc_epi}")
-        print(f"SC epicentre correlation: r = {r_sc_epi}, p = {p_sc_epi}")
-
-    print()
-    print("------------")
-    print("Save results")
-    print("------------")
-    util.save_to_pickle(
-        "../../data/results/04_epicentreAssociation/local_epicentre.pkl",
-        {"epicentre": epicentre},
-    )
-
-    util.save_to_pickle(
-        "../../data/results/04_epicentreAssociation/local_association.pkl",
-        {"association": association},
-    )
+        util.save_to_pickle(
+            f"../../data/results/04_epicentreAssociation/{site}_association.pkl",
+            {"association": association},
+        )
 
 
 if __name__ == "__main__":
